4c349336919512849d226b16953924c444f53a2e
chriskuz <cmkuzemka@gmail.com>
2021-07-14 23:17:51 -0400

    Added sources.

--- src/main.py
+++ src/main.py
@@ -10,5 +10,14 @@ class main:
         # Once user select option, require step process for the option for input credentials
         # Run function  
 
+    # https://realpython.com/python3-object-oriented-programming/
+    # https://docs.python-guide.org/writing/structure/
+    # https://pypi.org/project/selenium/
+    # https://automatetheboringstuff.com/chapter11/
+    # https://www.edureka.co/blog/web-scraping-with-python/
+    
+
+
+
 
     pass

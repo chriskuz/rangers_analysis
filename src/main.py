e55e235015165d36cd6aff2987bc97c0c1491d74
chriskuz <cmkuzemka@gmail.com>
2021-07-14 23:16:10 -0400

    Began mild structuring

---
+++ src/main.py
@@ -0,0 +1,14 @@
+class main:
+    def __init__(self):
+        pass
+    
+    # Initializing functions:
+    # Clear cache on initialization??
+
+    # Prep function Listing what the user wants to do
+        # Inputs will then be used within main to activate other functions 
+        # Once user select option, require step process for the option for input credentials
+        # Run function  
+
+
+    pass
